a47788ea48416b2bb42574fe323d78742f5e6161
kvnchpl <kvnchpl@gmail.com>
2025-12-27 17:01:21 -0500

    update

--- exp.js
+++ exp.js
@@ -4,3 +4,15 @@ if (idTarget) {
   const title = document.title.trim();
   idTarget.textContent = title;
 }
+
+document.addEventListener("contextmenu", (event) => {
+  if (event.target && event.target.closest("img")) {
+    event.preventDefault();
+  }
+});
+
+document.addEventListener("dragstart", (event) => {
+  if (event.target && event.target.closest("img")) {
+    event.preventDefault();
+  }
+});
