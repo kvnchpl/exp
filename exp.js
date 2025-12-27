ddfc0911e0b82595d2128711309c80a8babca94b
kvnchpl <kvnchpl@gmail.com>
2025-12-27 16:41:09 -0500

    update

--- exp.js
+++ exp.js
@@ -2,8 +2,5 @@ const idTarget = document.querySelector(".exp-page-id");
 
 if (idTarget) {
   const title = document.title.trim();
-  const normalized = title.toLowerCase();
-  idTarget.textContent = normalized.startsWith("exp ")
-    ? normalized
-    : `exp ${normalized}`;
+  idTarget.textContent = title;
 }
