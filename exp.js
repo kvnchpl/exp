5a4f07d852e6c50450efc67dfefa658bd91dc4be
kvnchpl <kvnchpl@gmail.com>
2026-02-27 19:02:49 -0500

    update

--- exp.js
+++ exp.js
@@ -1,6 +1,9 @@
+const TITLE_PREFIX = "EXP | ";
 const idTarget = document.querySelector(".exp-page-id");
 
 if (idTarget) {
   const title = document.title.trim();
-  idTarget.textContent = title;
+  idTarget.textContent = title.startsWith(TITLE_PREFIX)
+    ? title.slice(TITLE_PREFIX.length)
+    : title;
 }
